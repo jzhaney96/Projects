aa0cc4f82e7b13ef2cb0893413b34f391df39d49
jzhaney96 <113325819+jzhaney96@users.noreply.github.com>
2024-07-09 12:07:59 +0300

    Add files via upload
    To do list

---
+++ script.js
@@ -0,0 +1,21 @@
+function createTodo () {
+    const listItem = document.createElement('li');
+    const todoContainerElement = document.getElementById('todos-container');
+    todoContainerElement.appendChild(listItem);
+    const userInput = document.getElementById('note').value;
+    listItem.innerHTML = userInput;
+    listItem.setAttribute("onclick", "todoCheck(event, this)");
+    document.getElementById('note').value = '';
+}
+
+function deleteTodo () {
+    document.getElementById('todos-container').innerHTML = '';
+}
+
+function todoCheck(e,todo) {
+    if (todo.style.textDecoration === '') {
+        todo.style.textDecoration = 'line-through';}
+        else {
+            todo.style.textDecoration = '';
+        }
+}
